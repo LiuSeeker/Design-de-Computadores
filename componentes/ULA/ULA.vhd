35acee1b3f7268dd0c1cd1f76cf0488e83bd5445
IagoMendes <iagorm@al.insper.edu.br>
2019-10-03 20:40:37 -0300

    ula finalizada, faltam detalhes finais

--- componentes/ULA/ULA.vhd
+++ componentes/ULA/ULA.vhd
@@ -28,15 +28,16 @@ begin
 	processo: process(inA, inB, sel)
 	begin
 
-		if(sel = "000") then --inc (soma 1 no valor do reg)
+		if(sel = "000") then --inc (soma 1 no valor do reg) # definir onde entrar 
 			outData <= inA + "00000001";
 			flag <= '0';
 
 		elsif(sel = "001") then --inc2
-			
+			flag <= 0;
 			
 		elsif(sel = "010") then --cmp (compara imediato com registrador) ##################OLHAR
 			outData <= "00000000";
+			
 			if(inA = inB) then
 				flag <= '1';
 			else
@@ -44,17 +45,23 @@ begin
 			end if;
 
 		elsif(sel = "011") then --passA
+			outData <= inA;
+			flag <= '0';
 		
 		elsif(sel = "100") then --passB
+			outData <= inB;
+			flag <= '0';
 		
 		elsif(sel = "101") then --pass (ignora entrada)
+			outData <= "00000000";
+			flag <= '0';
 		
 		elsif(sel = "110") then --JMP (flag = 1)
+			outData <= "00000000";
 			flag <= '1';
 			
 		else -- (111) JMP NOT ZERO, mantem flag do compare
-			outData <= inB;
-			flag <= '0';
+			outData <= "00000000";
 
 		end if;
 	end process processo;
