477c4cc1619f0f4175fb97b60933fdc86f40849d
ehrhardt98 <jorgeavbe@al.insper.edu.br>
2019-10-07 12:22:49 -0300

    latches removidos??

--- componentes/ULA/ULA.vhd
+++ componentes/ULA/ULA.vhd
@@ -23,44 +23,50 @@ end entity;
 architecture ulaula of ula is
 begin
 
-	processo: process(inA, inB, sel)
-	begin
+	outData <= std_logic_vector(unsigned(inA) + "0001") when sel = "000" else
+				  "0000" when sel = "010" OR sel = "101" OR sel = "110" OR sel = "111";
+				  
+	flag <= '1' when (sel = "010" AND inA = inB) OR sel = "110" else
+			  '0';
+				  
+	--processo: process(inA, inB, sel)
+	--begin
 
-		if(sel = "000") then --inc (soma 1 no valor do reg) # definir onde entrar 
-			outData <= std_logic_vector(unsigned(inA) + "0001");
-			flag <= '0';
+--		if(sel = "000") then --inc (soma 1 no valor do reg) # definir onde entrar 
+--			outData <= std_logic_vector(unsigned(inA) + "0001");
+--			flag <= '0';
 
-		elsif(sel = "001") then --inc2
-			flag <= '0';
+--		elsif(sel = "001") then --inc2
+--			flag <= '0';
 			
-		elsif(sel = "010") then --cmp (compara imediato com registrador) ##################OLHAR
-			outData <= "0000";
+--		elsif(sel = "010") then --cmp (compara imediato com registrador) ##################OLHAR
+--			outData <= "0000";
 			
-			if(inA = inB) then
-				flag <= '1';
-			else
-				flag <= '0';  
-			end if;
+--			if(inA = inB) then
+--				flag <= '1';
+--			else
+--				flag <= '0';  
+--			end if;
 
-		elsif(sel = "011") then --passA
-			outData <= inA;
-			flag <= '0';
-		
-		elsif(sel = "100") then --passB
-			outData <= inB;
-			flag <= '0';
-		
-		elsif(sel = "101") then --pass (ignora entrada)
-			outData <= "0000";
-			flag <= '0';
-		
-		elsif(sel = "110") then --JMP (flag = 1)
-			outData <= "0000";
-			flag <= '1';
-			
-		else -- (111) JMP NOT ZERO, mantem flag do compare
-			outData <= "0000";
+--		elsif(sel = "011") then --passA
+--			outData <= inA;
+--			flag <= '0';
+--		
+--		elsif(sel = "100") then --passB
+--			outData <= inB;
+--			flag <= '0';
+--		
+--		elsif(sel = "101") then --pass (ignora entrada)
+--			outData <= "0000";
+--			flag <= '0';
+--		
+--		elsif(sel = "110") then --JMP (flag = 1)
+--			outData <= "0000";
+--			flag <= '1';
+--			
+--		else -- (111) JMP NOT ZERO, mantem flag do compare
+--			outData <= "0000";
 
-		end if;
-	end process processo;
+--		end if;
+--		end process processo;
 end architecture;
