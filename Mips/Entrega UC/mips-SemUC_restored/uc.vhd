5c754a598c6963974b578e7b950e1f2a7b4fd268
LiuSeeker <vitor.liu@gmail.com>
2019-11-13 08:22:50 -0300

    SSSSSSSS

---
+++ Mips/Entrega UC/mips-SemUC_restored/uc.vhd
@@ -0,0 +1,28 @@
+-- Design de Computadores
+-- file: uc.vhd
+-- date: 18/10/2019
+
+library ieee;
+use ieee.std_logic_1164.all;
+use work.constantesMIPS.all;
+
+entity uc is
+	port
+    (
+        opcode              	: IN STD_LOGIC_VECTOR(OPCODE_WIDTH-1 DOWNTO 0);
+        pontosDeControle    	: OUT STD_LOGIC_VECTOR(CONTROLWORD_WIDTH-1 DOWNTO 0)
+    );
+end entity;
+
+architecture bhv of uc is
+begin
+    process(opcode)
+	 begin
+	 case opcode is
+		when "000000" => pontosDeControle <= "XXX10001000",
+		when "100011" => pontosDeControle <= "XXX10110100",
+		when "101011" => pontosDeControle <= "XXX01000100",
+		when "000100" => pontosDeControle <= "XXX00000010",
+		when "000010" => pontosDeControle <= "XXX00000001"
+	 
+end bhv;
